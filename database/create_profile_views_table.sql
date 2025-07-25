e5b081125aed05dd2b4c3292ded28341847e416c
AloyeBolu <aloyebolu5@gmail.com>
2025-07-25 10:21:23 +0100

    first commit

--- database/create_profile_views_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE profile_views (
-  id SERIAL PRIMARY KEY,
-  profile_owner_id VARCHAR(255) NOT NULL,
-  viewer_id VARCHAR(255) NOT NULL,
-  viewed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT fk_profile_owner FOREIGN KEY (profile_owner_id) REFERENCES users(id),
-  CONSTRAINT fk_viewer FOREIGN KEY (viewer_id) REFERENCES users(id)
-);
